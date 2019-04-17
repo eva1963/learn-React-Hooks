92567fe00f07fde1852ca5daecf73fd6c213d599
刘欢 <sias_xg_217@126.com>
2019-04-17 11:07:48 +0800

    test

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,47 +1,42 @@
 import React from "react";
 
 interface HomeProps {
-    num: number;
-    plus: () => void;
+  num: number;
+  plus: () => void;
 }
 
-interface HomeStates extends HomeProps {};
+interface HomeStates extends HomeProps {}
 
 export class Home extends React.Component<HomeProps, HomeStates> {
-    state = {
-        num: this.props.num,
-        plus: this.props.plus
-    }
+  state = {
+    num: this.props.num,
+    plus: this.props.plus
+  };
 
-    componentDidMount() {
-        console.log(1);
-    }
+  componentDidMount() {
+    console.log(1);
+  }
 
-    static getDerivedStateFromProps(nextProps: HomeProps, prevstate: HomeStates) {
-        console.log("子组件的getDerived===>", nextProps, prevstate);
-        if (nextProps.num !== prevstate.num) {
-            return {
-                num: nextProps.num
-            }
-        }
-        return null;
+  static getDerivedStateFromProps(nextProps: HomeProps, prevstate: HomeStates) {
+    console.log("子组件的getDerived===>", nextProps, prevstate);
+    if (nextProps.num !== prevstate.num) {
+      return {
+        num: nextProps.num
+      };
     }
+    return null;
+  }
 
+  componentDidUpdate() {
+    console.log(2);
+  }
 
-    componentDidUpdate() {
-        console.log(2);
-    }
-
-    render() {
-        return (
-            <div>
-                <div>{this.state.num}</div>
-                <button onClick={this.props.plus}>
-                1
-                
-                plus 1
-                </button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div>{this.state.num}</div>
+        <button onClick={this.props.plus}>123 plus 1</button>
+      </div>
+    );
+  }
 }
