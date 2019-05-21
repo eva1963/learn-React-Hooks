8e9ae27024b3996fa91e5c9ca5ef4923997e8dc0
刘欢 <sias_xg_217@126.com>
2019-05-21 14:19:21 +0800

    优化配置信息

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,13 @@
 import * as React from "react";
 import "./App.scss";
-import { Home } from "./components/home";
-import { Child } from "./components/child";
-import { NewChild } from "./components/NewChild";
+import UState from "./components/usetState";
 
-interface Appstates {
-  num: number;
-}
-class App extends React.Component<{}, Appstates> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      num: 0
-    };
-  }
-
-  // componentWillMount() {
-  //   console.log("componentWillMount");
-  // }
-
-  // UNSAFE_componentWillMount() {
-  //   console.log("unsafe====>");
-  // }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        num: this.state.num + 1
-      });
-    }, 1000);
-  }
-
-  plus = () => {
-    // this.setState({
-    //   num: this.state.num + 1
-    // },() => {console.log(this.state.num);
-    // })
-    console.log("plus");
-  };
-
-  render() {
+class App extends React.Component<{}, {}> {
+  render(): JSX.Element {
     return (
       <div className="App">
-        {/* <h1>{this.state.num}</h1> */}
-        <Home num={this.state.num} plus={this.plus} />
-        {/* <Child/> */}
-        {/* <NewChild/> */}
-
-        {/* <button onClick={this.plus}>plus 1</button> */}
+        {/* useState && useEffect */}
+        <UState />
       </div>
     );
   }
